e6cd0add6d3f568d4069331da8798598255b2931
Praveen-2112 <praveenspr2112.com>
2023-06-10 10:48:44 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,156 @@
+//1)
+
+// const a=-48;
+// let b=Math.abs(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+
+
+
+// const a=true;
+// let b=Math.abs(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+
+// const a=false;
+// let b=Math.abs(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a="praveen";
+// let b=Math.abs(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+//math.round
+
+// const a="praveen";
+// let b=Math.round(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=18.5;
+// let b=Math.round(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=333;
+// let b=Math.round(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=18.5 -"praveen";
+// let b=Math.round(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=true;
+// let b=Math.round(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+//floor
+// const a=18.5;
+// let b=Math.floor(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+
+// const a=185;
+// let b=Math.floor(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=true;
+// let b=Math.floor(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+//sqrt
+
+
+// const a=18;
+// let b=Math.sqrt(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=18.7;
+// let b=Math.sqrt(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a="praveen";
+// let b=Math.sqrt(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=true;
+// let b=Math.sqrt(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=false;
+// let b=Math.sqrt(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+
+
+
+
+
+//ceil
+
+// const a=18;
+// let b=Math.ceil(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=18.7;
+// let b=Math.ceil(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a="praveen";
+// let b=Math.ceil(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=true;
+// let b=Math.ceil(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=false;
+// let b=Math.ceil(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+
+
+//random
+
+// const a=18;
+// let b=Math.random(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=18.7;
+// let b=Math.random(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a="praveen";
+// let b=Math.random(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=true;
+// let b=Math.random(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
+
+
+// const a=false;
+// let b=Math.random(a);
+// document.write(`input valuse is ${a}`,"<br>",`return value is ${b}`);
